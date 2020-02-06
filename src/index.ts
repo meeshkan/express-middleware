1900ab0a388afa5ad50979229ea75eae2613c6b2
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-02-06 09:09:55 +0200

    Add prettier config from meeshkan-codestyle.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import {
-  HttpMethod,
-  HttpProtocol,
-  HttpRequestBuilder,
-  HttpResponseBuilder
-} from "http-types";
+import { HttpMethod, HttpProtocol, HttpRequestBuilder, HttpResponseBuilder } from "http-types";
 
 interface Options {
   writer: (s: string) => void;
@@ -13,30 +8,18 @@ interface Options {
 type WriteCb = (error: Error | null | undefined) => void;
 type EndCb = () => void;
 
-export default (options: Options) => (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export default (options: Options) => (req: Request, res: Response, next: NextFunction) => {
   const oldWrite = res.write;
   const oldEnd = res.end;
 
   const chunks: Buffer[] = [];
 
-  res.write = (
-    thingOne: any,
-    thingTwo?: string | WriteCb,
-    thingThree?: WriteCb
-  ) => {
+  res.write = (thingOne: any, thingTwo?: string | WriteCb, thingThree?: WriteCb) => {
     chunks.push(Buffer.from(thingOne));
     return oldWrite.apply(res, [
       thingOne,
       typeof thingTwo === "string" ? thingTwo : "utf8",
-      thingThree === undefined &&
-      thingTwo !== undefined &&
-      typeof thingTwo !== "string"
-        ? thingTwo
-        : thingThree
+      thingThree === undefined && thingTwo !== undefined && typeof thingTwo !== "string" ? thingTwo : thingThree,
     ]);
   };
 
@@ -53,22 +36,18 @@ export default (options: Options) => (
       pathname: req.path,
       query: req.query,
       protocol: req.protocol.toUpperCase() as HttpProtocol,
-      body: typeof req.body === "string" ? req.body : JSON.stringify(req.body)
+      body: typeof req.body === "string" ? req.body : JSON.stringify(req.body),
     });
     const response = HttpResponseBuilder.from({
       statusCode: res.statusCode,
       body,
-      headers: res.getHeaders() as { string: string | string[] }
+      headers: res.getHeaders() as { string: string | string[] },
     });
     options.writer(JSON.stringify({ request, response }));
     return oldEnd.apply(res, [
       thingOne,
       typeof thingTwo === "string" ? thingTwo : "utf8",
-      thingThree === undefined &&
-      thingTwo !== undefined &&
-      typeof thingTwo !== "string"
-        ? thingTwo
-        : thingThree
+      thingThree === undefined && thingTwo !== undefined && typeof thingTwo !== "string" ? thingTwo : thingThree,
     ]);
   };
 
